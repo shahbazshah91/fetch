82854b1f7f4659d3e66963a16e98470c2995b486
shahbazshah91 <106367847+shahbazshah91@users.noreply.github.com>
2023-05-30 21:56:50 +0500

    Update fetch.sh

--- fetch.sh
+++ fetch.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 echo -e "This script will fetch the the full backup and will place it in the directory you're currently in.\n"
-echo -n "Enter the application name: "
-read website_name
+read -p "Enter the application name: " website_name
 read -p "Enter the date (YYYY-MM-DD): " date
 read -p "Enter the time (HH:MM:SS): " time_b
+
+echo -e ""
+echo "Size of Webfiles= $(du -sh /home/master/applications/$website_name/ | awk '{print $1}')  and  Size of DB= $(du -sh /var/lib/mysql/$website_name/ | awk '{print $1}')"
 
 datetime="${date}T${time_b}"
 command="/var/cw/scripts/bash/duplicity_restore.sh --src $website_name -r --dst './' --time '$datetime'"
